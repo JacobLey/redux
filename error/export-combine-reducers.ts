7827b0dda4cd6e109b0f8c0633041a6a6d2726a2
Jacob Ley <jacob.ley@showcasejobs.com>
2021-03-07 18:51:46 -0800

    Add extra cases of failure

--- error/export-combine-reducers.ts
+++ error/export-combine-reducers.ts
@@ -1,3 +1,18 @@
-import { combineReducers } from 'redux';
+import { combineReducers, createStore, Reducer } from 'redux';
 
-export default combineReducers<{}>({});
+const reducer: Reducer<
+    Record<string, number>,
+    { type: string, val: number }
+> = (state, action) => ({
+    ...state,
+    [action.type]: action.val,
+});
+
+// Fails
+export const withEmpty = combineReducers({});
+
+// Succeeds somehow...
+export const withMap = combineReducers({ reducer });
+
+// Fails
+export const store = createStore(withMap)
